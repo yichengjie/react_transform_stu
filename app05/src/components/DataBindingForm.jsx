c3fa1862fd3aca02c17be6fc3a9d9ba8be2d3b5e
yichengjie <626659321@qq.com>
2016-08-28 21:28:22 +0800

    advance

---
+++ app05/src/components/DataBindingForm.jsx
@@ -0,0 +1,134 @@
+/* eslint react/no-multi-comp:0, no-console:0 */
+
+import { createForm } from 'rc-form';
+import React, { PropTypes } from 'react';
+import { createRootContainer, createContainer } from 'react-data-binding';
+import ReactDOM from 'react-dom';
+import { regionStyle } from './styles';
+import Switch from 'antd/lib/switch';
+import './antd.css';
+
+const createFormContainer = createContainer((state) => {
+  return {
+    on: ((state.formState || {}).on || {}).value || false,
+  };
+});
+
+let TopForm = React.createClass({
+  propTypes: {
+    form: PropTypes.object,
+    on: PropTypes.bool,
+  },
+
+  render() {
+    const { form, on } = this.props;
+    const { getFieldProps } = form;
+    return (<div style={ regionStyle }>
+      <div>has email? </div>
+      <div>
+        <Switch {...getFieldProps('on', {
+          initialValue: on,
+          valuePropName: 'checked',
+        })}
+        /></div>
+    </div>);
+  },
+});
+TopForm = createFormContainer(TopForm);
+
+
+
+let BottomForm = React.createClass({
+  propTypes: {
+    form: PropTypes.object,
+    on: PropTypes.bool,
+  },
+
+  render() {
+    const { form, on } = this.props;
+    const style = {
+      ...regionStyle,
+      display: on ? 'block' : 'none',
+    };
+    return (<div style={ style }>
+      <div>email: </div>
+      <div>
+        <input {...form.getFieldProps('email', {
+          rules: [{
+            type: 'email',
+          }],
+          hidden: !on,
+        })}
+        /></div>
+    </div>);
+  },
+});
+
+BottomForm = createFormContainer(BottomForm);
+
+
+
+
+
+
+
+let Form = React.createClass({
+  propTypes: {
+    form: PropTypes.object,
+  },
+  onSubmit(e) {
+    e.preventDefault();
+    console.log(this.props.form.getFieldsValue());
+  },
+  render() {
+    const { form } = this.props;
+    return (<div>
+      <TopForm form={ form }/>
+      <BottomForm form={ form }/>
+      <div style={ regionStyle }>
+        <button onClick={this.onSubmit}>submit</button>
+      </div>
+    </div>);
+  },
+});
+
+Form = createForm({
+  mapPropsToFields(props) {
+    console.log('mapPropsToFields', props);
+    return props.formState;
+  },
+  onFieldsChange(props, fields) {
+    console.log('onFieldsChange', fields);
+    props.setStoreState({
+      formState: {
+        ...props.formState,
+        ...fields,
+      },
+    });
+  },
+})(Form);
+
+Form = createContainer((state) => {
+  return {
+    formState: state.formState,
+  };
+})(Form);
+
+class App extends React.Component {
+  render() {
+    return (<div>
+      <h2>integrate with react-data-binding</h2>
+      <Form />
+    </div>);
+  }
+}
+
+const NewApp = createRootContainer({
+  formState: {
+    on: { value: true },
+    email:{value:'123@qq.com'},
+  },
+})(App);
+
+export default NewApp ;
+//ReactDOM.render(<NewApp />, document.getElementById('__react-content'));
--- app05/src/index.jsx
+++ app05/src/index.jsx
@@ -4,13 +4,13 @@ import SimpleForm from './components/SimpleForm.jsx' ;
 import SetFieldsValue from './components/SetFieldsValue.jsx' ;
 import ValidateFirst from './components/ValidateFirst.jsx' ;
 import AsyncInit from './components/AsyncInit.jsx' ;
-import DataBding from './components/DataBinding.jsx' ;
-
+import DataBinding from './components/DataBinding.jsx' ;
+import DataBindingForm from './components/DataBindingForm.jsx' ;
 
 var el = document.createElement('div') ;
 el.id="app";
 document.body.appendChild(el) ;
 
 ReactDOM.render(
-  <SimpleForm />,
+  <DataBinding />,
   document.getElementById('app'));
