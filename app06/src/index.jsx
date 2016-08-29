40957f2250b1ebd7d521be15284a2220239682e8
yichengjie <626659321@qq.com>
2016-08-29 11:39:48 +0800

    t

--- app06/src/index.jsx
+++ app06/src/index.jsx
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom';
 import User from './components/User.jsx' ;
 import StatelessUser from './components/StatelessUser.jsx' ;
 import ReduxUser from './components/ReduxUser.jsx' ;
-
+import ReduxUser2 from './components/ReduxUser2.jsx' ;
 var el = document.createElement('div') ;
 el.id="app";
 document.body.appendChild(el) ;
 
 ReactDOM.render(
-      <ReduxUser />,
+      <ReduxUser2 />,
  document.getElementById('app'));
