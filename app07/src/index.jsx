164e7ace50e1576e748b8295b2e27c555c10a186
yichengjie <626659321@qq.com>
2016-08-29 15:16:35 +0800

    advance

--- app07/src/index.jsx
+++ app07/src/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ValidateBasic from './components/ValidateBasic.jsx' ;
+//import znCH from 'antd/lib/locale-provider/en_US';
 
 var el = document.createElement('div') ;
 el.id="app";
