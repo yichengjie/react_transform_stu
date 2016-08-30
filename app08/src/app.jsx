02a552e7905ad5864596d8bf336e64fe68a8735e
yichengjie <626659321@qq.com>
2016-08-30 14:30:29 +0800

    t

--- app08/src/app.jsx
+++ app08/src/app.jsx
@@ -1,14 +1,23 @@
-import ValidateBasic from './containers/ValidateBasic.js' ;
 import React,{Component} from 'react';
+import ValidateBasic from './containers/ValidateBasic.js' ;
+import { Button,Row, Col} from 'antd';
 
 export default class App extends Component {
   constructor(props) {
     super(props) ;
   }
+  handleSubmit(e){
+    console.info('提交表单') ;
+  }
   render(){
     return (
       <div className = "container">
-        <ValidateBasic/>
+        <Row>
+          <Col span={7}></Col>
+          <Col span={5}><Button type="primary" onClick={this.handleSubmit.bind(this)}>submit</Button></Col>
+        </Row>
+        <br/>
+        <ValidateBasic />
       </div>
     ) ;
   }
