ad316bcdb9b7f52aaef63fa9c84b2e4cd844ea4c
yichengjie <626659321@qq.com>
2016-08-31 15:08:25 +0800

    t

--- app09/src/index.jsx
+++ app09/src/index.jsx
@@ -5,10 +5,10 @@ import App from './app.jsx' ;
 import store from './stores/store.js' ;
 import {Provider} from 'react-redux' ;
 
- var el = document.createElement('div') ;
- el.id="app";
- el.style.margin = "80px 0 0 50px" ;
- document.body.appendChild(el) ;
+var el = document.createElement('div') ;
+el.id="app";
+el.style.margin = "80px 0 0 50px" ;
+document.body.appendChild(el) ;
 
 ReactDOM.render(
     <Provider store={store}>
