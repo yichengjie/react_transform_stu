5e41057fa1d67242f13c401154328f9fa97af10b
yichengjie <626659321@qq.com>
2016-09-07 16:39:40 +0800

    backups

--- app11/src/views/query/BrandItem.jsx
+++ app11/src/views/query/BrandItem.jsx
@@ -5,21 +5,27 @@ class BrandItem extends Component{
     render(){
         return (
             <li className="brand_list">
-                <div className="brand_list_item">
+                <div className="left item1">
                     1
                 </div>
-                <div className="brand_list_item">
+                <div className="left">
                     <span className="brand_img brand_img_ca"></span>
                 </div>
-                <div className="brand_list_item">
+                <div className="left">
                     <p className="brand_name">xx001</p>
                     <p>层级序号：10011</p>
                     <p>测试一下啊这里的描述信息</p>
                 </div>
-                <div className="pull-right">
-                    <p><Icon type="info-circle-o"/></p>
-                    <p><Icon type="edit"/></p>
-                    <p><Icon type="delete"/></p>
+                <div className="right">
+                    <p>
+                        <Icon type="info-circle-o myhand"/>
+                    </p>
+                    <p>
+                        <Icon type="edit myhand"/>
+                    </p>
+                    <p>
+                        <Icon type="delete myhand"/>
+                    </p>
                 </div>
             </li>
         ) ;
