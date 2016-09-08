302ca3f0c204a9bebbf360412c01bd7e76987b19
yichengjie <626659321@qq.com>
2016-09-08 10:18:25 +0800

    backups

--- app11/src/index.jsx
+++ app11/src/index.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import AppLayout from './views/query/AppLayout.jsx' ;
 import store from './stores/store.js' ;
 import {Provider} from 'react-redux' ;
-var mountNode = document.getElementById('app') ;
+import AppLayout from './views/query/AppLayout.jsx' ;
+
 
 ReactDOM.render(
     <Provider store={store}>
       <AppLayout />
     </Provider>,
-    mountNode);
+    document.getElementById('app'));
