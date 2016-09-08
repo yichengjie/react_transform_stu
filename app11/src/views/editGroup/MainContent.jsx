dbe9e9010859ab8ac235d6054a9e2d28712db594
yichengjie <626659321@qq.com>
2016-09-08 16:17:49 +0800

    commit

--- app11/src/views/editGroup/MainContent.jsx
+++ app11/src/views/editGroup/MainContent.jsx
@@ -1,11 +1,21 @@
 import React,{Component} from 'react' ;
+import {Card} from 'antd' ;
 
 class MainContent extends Component{
 
     render(){
         return (
             <div className ="container">
-                main content
+                <Card title="品牌集">
+                    <p>main content</p>
+                    <p>main content</p>
+                    <p>main content</p>
+                    <p>main content</p>
+                    <p>main content</p>
+                    <p>main content</p>
+                    <p>main content</p>
+                    <p>main content</p>
+                </Card>
             </div>
         ) ;
     }
