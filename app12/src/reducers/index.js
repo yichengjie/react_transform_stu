08d30d3117abeffa136f7f03e45d8d35a8aa8ecc
yichengjie <626659321@qq.com>
2016-09-13 14:56:25 +0800

    删除无效文件

--- app12/src/reducers/index.js
+++ app12/src/reducers/index.js
@@ -1,8 +1,6 @@
 import { combineReducers } from 'redux' ;
-import form from './form.js' ;
 import subTableList from './subTableList.js' ;
 
 export default combineReducers({
-  form,
   subTableList
 });
